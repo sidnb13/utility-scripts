277adbae5c57b53882a4d234410dbbef4b338d02
sidnb13 <62273881+sidnb13@users.noreply.github.com>
2020-08-15 20:35:53 -0500

    fetch bitwise or

--- bash-scripts/fetch-all-repos.sh
+++ bash-scripts/fetch-all-repos.sh
@@ -5,7 +5,7 @@ git_fetch() {
         cd $1
         {
             {
-                git pull --rebase
+                git pull --rebase || git fetch
             } && {
                 echo "$(date +"%H:%M:%S %m/%d/%y") - $(basename $BASH_SOURCE) - $1 fetched" >> /Users/sidbaskaran/Desktop/utility-scripts/MASTER.log
             }
